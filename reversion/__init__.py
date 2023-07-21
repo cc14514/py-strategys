89b02e1ef48eb7b3fc5a66bbcc9672b93191468f
cc14514 <cc14514@icloud.com>
2023-07-21 18:33:34 +0800

    debug

--- reversion/__init__.py
+++ reversion/__init__.py
@@ -3,28 +3,53 @@
 # pip install websocket-client
 # pip3 install numpy
 
-#Real Trading: wss://fx-ws.gateio.ws/v4/ws/btc
-#TestNet Trading: wss://fx-ws-testnet.gateio.ws/v4/ws/btc
-ws_api_path = 'wss://fx-ws-testnet.gateio.ws/v4/ws/btc'
+"""
+币安：
 
-proxy_host = '127.0.0.1'  
-proxy_port = 9981 
+https://binance-docs.github.io/apidocs/futures/cn/#api
 
-ws = None
+合约类型 (contractType):
 
-def connect_to_websocket() :
-    global ws  
-    ws = create_connection(ws_api_path, http_proxy_host=proxy_host, http_proxy_port=proxy_port)
-
-def close_websocket():
-    global ws  
-    if ws is not None:
-        ws.close()
-        ws = None  
+PERPETUAL 永续合约
+CURRENT_MONTH 当月交割合约
+NEXT_MONTH 次月交割合约
+CURRENT_QUARTER 当季交割合约
+NEXT_QUARTER 次季交割合约
+PERPETUAL_DELIVERING 交割结算中合约
 
 
-# ws.send('{"time" : 123456, "channel" : "futures.ping"}')
+订单种类 (orderTypes, type):
 
-# ws.send('{"time" : 123456, "channel" : "futures.tickers", "event": "subscribe", "payload" : ["BTC_USD"]}')
+LIMIT 限价单
+MARKET 市价单
+STOP 止损限价单
+STOP_MARKET 止损市价单
+TAKE_PROFIT 止盈限价单
+TAKE_PROFIT_MARKET 止盈市价单
+TRAILING_STOP_MARKET 跟踪止损单
 
-# ws.recv()
+
+订单方向 (side):
+
+BUY 买入
+SELL 卖出
+
+有效方式 (timeInForce):
+
+GTC - Good Till Cancel 成交为止
+IOC - Immediate or Cancel 无法立即成交(吃单)的部分就撤销
+FOK - Fill or Kill 无法全部立即成交就撤销
+GTX - Good Till Crossing 无法成为挂单方就撤销
+
+条件价格触发类型 (workingType)
+
+MARK_PRICE
+CONTRACT_PRICE
+
+testnet的 REST baseurl 为 "https://testnet.binancefuture.com"
+testnet的 Websocket baseurl 为 "wss://stream.binancefuture.com"
+"""
+
+
+
+
